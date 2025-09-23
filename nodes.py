4971ab8918f439b3b7f64abbb6c8abab93a75fe3
lrzjason <lrzjason@users.noreply.github.com>
2025-09-23 13:38:47 +0800

    update

--- nodes.py
+++ nodes.py
@@ -49,28 +49,28 @@ class TextEncodeQwenImageEditPlus_lrzjason:
         for i, image in enumerate(images):
             if image is not None:
                 samples = image.movedim(-1, 1)
-                total = int(384 * 384)
-                scale_by = 1
-                if enable_vl_resize:
+                total = int(1024 * 1024)
+                if enable_resize:
                     scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
-                width = round(samples.shape[3] * scale_by)
-                height = round(samples.shape[2] * scale_by)
-
-                s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
-                image = s.movedim(1, -1)
-                images_vl.append(image)
+                width = round(samples.shape[3] * scale_by / 8.0) * 8
+                height = round(samples.shape[2] * scale_by / 8.0) * 8
                 if vae is not None:
-                    total = int(1024 * 1024)
                     scale_by = 1
-                    if enable_resize:
-                        scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
-                    width = round(samples.shape[3] * scale_by / 8.0) * 8
-                    height = round(samples.shape[2] * scale_by / 8.0) * 8
                     s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
                     image = s.movedim(1, -1)
                     ref_latents.append(vae.encode(image[:, :, :, :3]))
                     vae_images.append(image)
                 image_prompt += "Picture {}: <|vision_start|><|image_pad|><|vision_end|>".format(i + 1)
+                # print("before enable_vl_resize scale_by", scale_by)
+                if enable_vl_resize:
+                    total = int(384 * 384)
+                    scale_by = math.sqrt(total / (samples.shape[3] * samples.shape[2]))
+                    width = round(samples.shape[3] * scale_by)
+                    height = round(samples.shape[2] * scale_by)
+                # print("after enable_vl_resize scale_by", scale_by)
+                s = comfy.utils.common_upscale(samples, width, height, "area", "disabled")
+                image = s.movedim(1, -1)
+                images_vl.append(image)
 
         tokens = clip.tokenize(image_prompt + prompt, images=images_vl, llama_template=llama_template)
         conditioning = clip.encode_from_tokens_scheduled(tokens)
