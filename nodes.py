49338b7bd9d692a452dfe43b0c115e4a3db56b1b
lrzjason <lrzjason@users.noreply.github.com>
2025-09-27 02:06:57 +0800

    update

--- nodes.py
+++ nodes.py
@@ -114,11 +114,161 @@ class TextEncodeQwenImageEditPlus_lrzjason:
         o_image1, o_image2, o_image3, o_image4, o_image5 = vae_images
         return (conditioning, o_image1, o_image2, o_image3, o_image4, o_image5, latent_out)
 
+
+
+class TextEncodeQwenImageEditPlusAdvance_lrzjason:
+    upscale_methods = ["lanczos", "bicubic", "area"]
+    crop_methods = ["center", "disabled"]
+    target_sizes = [1024, 1344, 1536, 2048, 768, 512]
+    target_vl_sizes = [384]
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": 
+            {
+                "clip": ("CLIP", ),
+                "prompt": ("STRING", {"multiline": True, "dynamicPrompts": True}),
+            },
+            "optional": 
+            {
+                "vae": ("VAE", ),
+                "vl_resize_image1": ("IMAGE", ),
+                "vl_resize_image2": ("IMAGE", ),
+                "vl_resize_image3": ("IMAGE", ),
+                "not_resize_image1": ("IMAGE", ),
+                "not_resize_image2": ("IMAGE", ),
+                "not_resize_image3": ("IMAGE", ),
+                "target_size": (s.target_sizes, {"default": 1024}),
+                "target_vl_size": (s.target_vl_sizes, {"default": 384}),
+                "upscale_method": (s.upscale_methods,),
+                "crop": (s.crop_methods,),
+                "instruction": ("STRING", {"multiline": True, "default": "Describe the key features of the input image (color, shape, size, texture, objects, background), then explain how the user's text instruction should alter or modify the image. Generate a new image that meets the user's requirements while maintaining consistency with the original input where appropriate."}),
+            }
+        }
+
+    RETURN_TYPES = ("CONDITIONING", "LATENT", "IMAGE", "IMAGE", "IMAGE", "IMAGE", "IMAGE", "IMAGE", )
+    RETURN_NAMES = ("conditioning", "latent", "target_image1", "target_image2", "target_image3", "vl_resized_image1", "vl_resized_image2", "vl_resized_image3")
+    FUNCTION = "encode"
+
+    CATEGORY = "advanced/conditioning"
+
+    def encode(self, clip, prompt, vae=None, 
+               vl_resize_image1=None, vl_resize_image2=None, vl_resize_image3=None,
+               not_resize_image1=None, not_resize_image2=None, not_resize_image3=None, 
+               target_size=1024, 
+               target_vl_size=384,
+               upscale_method="lanczos",
+               crop="center",
+               instruction="",
+               ):
+        ref_latents = []
+        images = [not_resize_image1, not_resize_image2, not_resize_image3, 
+                  vl_resize_image1, vl_resize_image2, vl_resize_image3]
+        vl_resized_images = []
+        
+        images = [
+            {
+                "image": not_resize_image1,
+                "vl_resize": False 
+            },
+            {
+                "image": not_resize_image2,
+                "vl_resize": False 
+            },
+            {
+                "image": not_resize_image3,
+                "vl_resize": False 
+            },
+            {
+                "image": vl_resize_image1,
+                "vl_resize": True 
+            },
+            {
+                "image": vl_resize_image2,
+                "vl_resize": True 
+            },
+            {
+                "image": vl_resize_image3,
+                "vl_resize": True 
+            }
+        ]
+        
+        vae_images = []
+        vl_images = []
+        template_prefix = "<|im_start|>system\n"
+        template_suffix = "<|im_end|>\n<|im_start|>user\n{}<|im_end|>\n<|im_start|>assistant\n"
+        instruction_content = ""
+        if instruction == "":
+            instruction_content = "Describe the key features of the input image (color, shape, size, texture, objects, background), then explain how the user's text instruction should alter or modify the image. Generate a new image that meets the user's requirements while maintaining consistency with the original input where appropriate."
+        else:
+            # for handling mis use of instruction
+            if template_prefix in instruction:
+                # remove prefix from instruction
+                instruction = instruction.split(template_prefix)[1]
+            if template_suffix in instruction:
+                # remove suffix from instruction
+                instruction = instruction.split(template_suffix)[0]
+            if "{}" in instruction:
+                # remove {} from instruction
+                instruction = instruction.replace("{}", "")
+            instruction_content = instruction
+        llama_template = template_prefix + instruction_content + template_suffix
+        image_prompt = ""
+
+        if vae is not None:
+            for i, image_obj in enumerate(images):
+                image = image_obj["image"]
+                vl_resize = image_obj["vl_resize"]
+                if image is not None:
+                    samples = image.movedim(-1, 1)
+                    current_total = (samples.shape[3] * samples.shape[2])
+                    total = int(target_size * target_size)
+                    scale_by = math.sqrt(total / current_total)
+                    width = round(samples.shape[3] * scale_by / 64.0) * 64
+                    height = round(samples.shape[2] * scale_by / 64.0) * 64
+                    s = comfy.utils.common_upscale(samples, width, height, upscale_method, crop)
+                    image = s.movedim(1, -1)
+                    ref_latents.append(vae.encode(image[:, :, :, :3]))
+                    vae_images.append(image)
+                    
+                    if vl_resize:
+                        total = int(target_vl_size * target_vl_size)
+                        scale_by = math.sqrt(total / current_total)
+                        width = round(samples.shape[3] * scale_by / 2) *2
+                        height = round(samples.shape[2] * scale_by / 2) *2
+                        s = comfy.utils.common_upscale(samples, width, height, upscale_method, crop)
+                        image = s.movedim(1, -1)
+                        vl_resized_images.append(image)
+                    # handle non resize vl images
+                    image_prompt += "Picture {}: <|vision_start|><|image_pad|><|vision_end|>".format(i + 1)
+                    vl_images.append(image)
+                    
+                
+        tokens = clip.tokenize(image_prompt + prompt, images=vl_images, llama_template=llama_template)
+        conditioning = clip.encode_from_tokens_scheduled(tokens)
+        if len(ref_latents) > 0:
+            conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": ref_latents}, append=True)
+        # Return latent of first image if available, otherwise return empty latent
+        samples = ref_latents[0] if len(ref_latents) > 0 else torch.zeros(1, 4, 128, 128)
+        latent_out = {"samples": samples}
+        if len(vae_images) < 3:
+            vae_images.extend([None] * (3 - len(vae_images)))
+        o_image1, o_image2, o_image3 = vae_images
+        
+        if len(vl_resized_images) < 3:
+            vl_resized_images.extend([None] * (3 - len(vl_resized_images)))
+        vl_image1, vl_image2, vl_image3 = vl_resized_images
+        
+        return (conditioning, latent_out, o_image1, o_image2, o_image3, vl_image1, vl_image2, vl_image3)
+
+
 NODE_CLASS_MAPPINGS = {
-    "TextEncodeQwenImageEditPlus_lrzjason": TextEncodeQwenImageEditPlus_lrzjason
+    "TextEncodeQwenImageEditPlus_lrzjason": TextEncodeQwenImageEditPlus_lrzjason,
+    "TextEncodeQwenImageEditPlusAdvance_lrzjason": TextEncodeQwenImageEditPlusAdvance_lrzjason
 }
 
 # Display name mappings
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "TextEncodeQwenImageEditPlus_lrzjason": "TextEncodeQwenImageEditPlus 小志Jason(xiaozhijason)"
+    "TextEncodeQwenImageEditPlus_lrzjason": "TextEncodeQwenImageEditPlus 小志Jason(xiaozhijason)",
+    "TextEncodeQwenImageEditPlusAdvance_lrzjason": "TextEncodeQwenImageEditPlusAdvance 小志Jason(xiaozhijason)"
 }
